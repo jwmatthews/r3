945d7f33c0a5bfb61c3c2f529a05e4964dcab2c1
John Matthews <jwmatthews@gmail.com>
2014-03-25 14:31:04 -0400

    Added a hack of restarting 'httpd' allows Pulp to function. Seeing exception: I'm seeing this on a fresh install of a Fedora 20 VM. Note, if I run "systemctl restart httpd" Pulp is happy and I'm able to login and create/sync a repo. It's the initial run of 'httpd' and the celery workers which is triggering the below.
    Mar 25 14:13:13 localhost pulp: pulp.server.webservices.application:INFO: *************************************************************
    Mar 25 14:13:13 localhost pulp: pulp.server.webservices.application:INFO: The Pulp server has been successfully initialized
    Mar 25 14:13:13 localhost pulp: pulp.server.webservices.application:INFO: *************************************************************
    Mar 25 14:13:13 localhost kernel: [  200.368118] httpd[2788]: segfault at 58 ip 00007fc73f9d4797 sp 00007fc7332316c0 error 4 in libpython2.7.so.1.0[7fc73f8f5000+178000]
    Mar 25 14:13:13 localhost kernel: httpd[2788]: segfault at 58 ip 00007fc73f9d4797 sp 00007fc7332316c0 error 4 in libpython2.7.so.1.0[7fc73f8f5000+178000]
    Mar 25 14:13:13 localhost pulp: pulp.server.webservices.application:CRITICAL: *************************************************************
    Mar 25 14:13:13 localhost pulp: pulp.server.webservices.application:ERROR: The Pulp server encountered an unexpected failure during initialization
    Traceback (most recent call last):
      File "/usr/lib/python2.7/site-packages/pulp/server/webservices/application.py", line 151, in wsgi_application
        _initialize_pulp()
      File "/usr/lib/python2.7/site-packages/pulp/server/webservices/application.py", line 115, in _initialize_pulp
        user_manager.ensure_admin()
      File "/usr/lib/python2.7/site-packages/pulp/server/managers/auth/user/cud.py", line 214, in ensure_admin
        password=default_password)
      File "/usr/lib/python2.7/site-packages/pulp/server/managers/auth/user/cud.py", line 88, in create_user
        User.get_collection().save(create_me, safe=True)
      File "/usr/lib/python2.7/site-packages/pulp/server/db/connection.py", line 135, in retry
        return method(*args, **kwargs)
      File "/usr/lib64/python2.7/site-packages/pymongo/collection.py", line 269, in save
        manipulate, safe, check_keys=check_keys, **kwargs)
      File "/usr/lib/python2.7/site-packages/pulp/server/db/connection.py", line 135, in retry
        return method(*args, **kwargs)
      File "/usr/lib64/python2.7/site-packages/pymongo/collection.py", line 479, in update
        check_keys, self.__uuid_subtype), safe)
      File "/usr/lib/python2.7/site-packages/pulp/server/db/connection.py", line 166, in _with_end_request
        return method(*args, **kwargs)
      File "/usr/lib64/python2.7/site-packages/pymongo/mongo_client.py", line 920, in _send_message
        rv = self.__check_response_to_last_error(response)
      File "/usr/lib64/python2.7/site-packages/pymongo/mongo_client.py", line 861, in __check_response_to_last_error
        raise DuplicateKeyError(details["err"], details["code"])
    DuplicateKeyError: E11000 duplicate key error index: pulp_database.users.$login_-1  dup key: { : "admin" }

--- devel-env/fedora/create_simple_repo_and_sync.sh
+++ devel-env/fedora/create_simple_repo_and_sync.sh
@@ -2,4 +2,4 @@
 
 pulp-admin login --username admin --password admin
 pulp-admin rpm repo create --repo-id pulp_unittest --feed http://jmatthews.fedorapeople.org/pulp_unittest/
-pulp-admin rpm repo sync run --repo-id pulp_unittest -bg
+pulp-admin rpm repo sync run --repo-id pulp_unittest
--- devel-env/fedora/setup_pulp.sh
+++ devel-env/fedora/setup_pulp.sh
@@ -4,13 +4,10 @@ sed -i "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
 
 yum install -y mongodb mongodb-server
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 yum install -y qpid-cpp-server
 echo "auth=no" >> /etc/qpidd.conf
-
-systemctl enable qpidd 
-systemctl start qpidd
 
 yum install -y wget
 wget http://repos.fedorapeople.org/repos/pulp/pulp/fedora-pulp.repo -O /etc/yum.repos.d/fedora-pulp.repo
@@ -19,18 +16,34 @@ sudo -u apache pulp-manage-db
 
 ./config_pulp.sh 
 
-systemctl enable pulp_workers.service
-systemctl start pulp_workers.service 
-
-systemctl enable pulp_celerybeat.service
-systemctl start pulp_celerybeat.service
-
-systemctl enable pulp_resource_manager.service
-systemctl start pulp_resource_manager.service
+systemctl enable qpidd 
+systemctl restart qpidd
 
 systemctl enable httpd
-systemctl start httpd
+systemctl restart httpd
 
+systemctl enable pulp_workers.service
+systemctl restart pulp_workers.service 
+
+systemctl enable pulp_resource_manager.service
+systemctl restart pulp_resource_manager.service
+
+systemctl enable pulp_celerybeat.service
+systemctl restart pulp_celerybeat.service
+
+##
+## Hack:  restart httpd again
+# I have noticed errors if I don't restart httpd again, after prior block of services started.
+# Only error I saw in /var/log/messages was the below issue:
+#  DuplicateKeyError: E11000 duplicate key error index: pulp_database.users.$login_-1  dup key: { : "admin" }
+#  Mar 25 14:03:52 localhost pulp: pulp.server.webservices.application:CRITICAL: *************************************************************
+#  Mar 25 14:03:52 localhost pulp: pulp.server.webservices.application:ERROR: The Pulp server encountered an unexpected failure during initialization
+#
+# If I restart httpd then pulp is happy.
+# I saw this behavior with pulp RPM: pulp-server-2.4.0-0.6.alpha.fc20.noarch
+##
+systemctl restart httpd
+./create_simple_repo_and_sync.sh
 
 echo ""
 echo "======="
