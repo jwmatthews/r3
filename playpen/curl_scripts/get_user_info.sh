5ecac34125cad5e822c4f5c69023cbb358cebdb8
John Matthews <jwmatthews@gmail.com>
2014-03-26 16:56:50 -0400

    Simple script to test basic auth and talking to pulp REST API

---
+++ playpen/curl_scripts/get_user_info.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export SERVER="172.31.2.53"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 username password"
+    exit 1
+fi
+
+export USERNAME=$1
+export PASSWORD=$2
+
+export AUTH=`python -c "import base64; print base64.encodestring(\"${USERNAME}:${PASSWORD}\")[:-1]"`
+
+
+curl -s -S -k -H "Authorization: Basic $AUTH" -X GET https://${SERVER}/pulp/api/v2/users/${USERNAME}/ | python -mjson.tool
+
+
