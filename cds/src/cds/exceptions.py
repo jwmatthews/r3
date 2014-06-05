11becfe54a5bb86324113523492cf517bf9f92d2
John Matthews <jwmatthews@gmail.com>
2014-06-05 15:08:58 -0400

    First steps to a CDS REST API wsgi app  - Hello world is server from Apache using mod_wsgi  - Accounted for issue with python-flask on el6 and jinja2 not importing correctly

---
+++ cds/src/cds/exceptions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2014 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License
+# (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied, including the
+# implied warranties of MERCHANTABILITY, NON-INFRINGEMENT, or FITNESS FOR A
+# PARTICULAR PURPOSE.
+# You should have received a copy of GPLv2 along with this software; if not,
+# see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+"""
+Exception classes thrown by CDS under error conditions.
+"""
+
+from flask import jsonify
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
