9ce70ab5470f3e5b797548874d6ef1349b598190
David Gao <jinmaster923@gmail.com>
2014-06-25 12:48:40 -0400

    Initial update for producing rsync list based on repo ids provided.

--- cds/playpen/rsync/generate_rsync_list.py
+++ cds/playpen/rsync/generate_rsync_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import os
+import socket
 import sys
 from optparse import OptionParser
+from pulp.bindings.server import PulpConnection 
+from pulp.bindings.actions import ActionsAPI
+from pulp.bindings.repository import RepositoryDistributorAPI
 from Queue import Queue
 
 def get_parser():
@@ -10,6 +14,8 @@ def get_parser():
                         help="Source path")
     parser.add_option("-o", "--out", dest="out_filename", default="rsync_list.txt",
                         help="Output filename")
+    parser.add_option("-i", "--repo-ids", dest="repo_ids",
+                        help="Comma separated repo ids")
     return parser
 
 def update_seen(path, seen):
@@ -114,22 +120,68 @@ def write_output_file(links, files, empty_dirs, out_filename):
     f.close()
     return
 
-if __name__ == "__main__":
-    parser = get_parser()
-    (opts, args) = parser.parse_args()
-    out_filename = opts.out_filename
-    source_path = opts.source
-    if source_path == None:
-        parser.print_help()
-        print "Please re-run with a source path provided"
-        sys.exit(1)
-    
-    links, files, empty_dirs, seen = process_path(source_path)
-    write_output_file(links, files, empty_dirs, out_filename)
+def get_repo_relative_paths(repo_ids, source_path):
+    # Since pulp-admin does NOT provide any mean to get specific repo information, 
+    # it's easier to access information via. pulp api calls.
 
+    # Initiate pulp server calls
+    relative_paths = []
+    hostname = socket.gethostname()
+    server = PulpConnection(hostname)
+    login_api = ActionsAPI(server)
+    distributor_api = RepositoryDistributorAPI(server)
+    login_api.login("admin", "admin") #TODO: hardcode for now, move as options later
+    for id in repo_ids.split(","):
+        print "Getting information on repo %s" % id
+        distributor_response = distributor_api.distributors(id)
+        if distributor_response != None or distributor_response.response_code == 200:
+            distributors = distributor_response.response_body
+            yum_distributor = [d for d in distributors if d['id'] == 'yum_distributor'][0]
+            relative_path = yum_distributor['config']['relative_url']
+            relative_paths.append("%s/%s" % (source_path, relative_path))
+        else:
+            print "Cannot find distributor for repo %s\n" % id
+            print "Unable to determine relative path for repo %s\n" % id
+           
+    return relative_paths
+
+def print_outputs(out_filename, files, empty_dirs, links):
     print "Results written to: %s" % (out_filename)
     print "Found:"
     print "\t %s Files" % (len(files))
     print "\t %s Empty Directories" % (len(empty_dirs))
     print "\t %s Symbolic Links" % (len(links))
+
+if __name__ == "__main__":
+    parser = get_parser()
+    (opts, args) = parser.parse_args()
+    out_filename = opts.out_filename
+    source_path = opts.source
+    repo_ids = opts.repo_ids
+    if source_path == None and repo_ids == None:
+        parser.print_help()
+        print "Please re-run with a source path or repo ids provided"
+        sys.exit(1)
+    else:
+        if repo_ids == None:
+            print "No repo-id specified, processing entire %s directory." % source_path
+            links, files, empty_dirs, seen = process_path(source_path)
+            write_output_file(links, files, empty_dirs, out_filename)
+            print_outputs(out_filename, files, empty_dirs, links)
+        else:
+            all_links = set()
+            all_files = set()
+            all_empty_dirs = set()
+            all_seen = set()
+            for repo_source_path in get_repo_relative_paths(repo_ids, source_path):
+                print "Processing repo path %s." % repo_source_path
+                links, files, empty_dirs, seen = process_path(repo_source_path)
+                # combine links, files, empty_dirs, seen with previous results
+                all_links.update(links)
+                all_files.update(files)
+                all_empty_dirs.update(empty_dirs)
+                all_seen.update(seen)
+            write_output_file(all_links, all_files, all_empty_dirs, out_filename)
+            print_outputs(out_filename, all_files, all_empty_dirs, all_links)
+                 
 
